b55ef7fc09551d9cf16b000a6c5162de53cdbba0
inyourface34456 m <trenten_miller@etownstudents.org>
2024-04-30 12:37:47 +0000

    i dont even know

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -5,7 +5,7 @@ int main(int argc, char* argv[]) {
 	std::cout << argc << "\n";
 	std::cout << argv << "\n";
 	std::vector<int> test = {10, 20, 30, 40};
-	
+
 	for (int i: test) {
 		std::cout << i << "\n";
 	}
